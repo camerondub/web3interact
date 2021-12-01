c7c334b5546c34421081f5873fec5479838126dc
Cameron Wong <cw@reosource.io>
2021-12-01 11:50:04 -0800

    Root commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+from setuptools_scm import get_version
+
+setup(
+    name="web3interact",
+    version=get_version(),
+    author="Cameron Wong",
+    description="",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "python-decouple",
+        "rlog @ git+https://github.com/camerondub/python-rlog.git#egg=rlog",
+        "py-solc-x",
+        "web3",
+    ],
+    entry_points={"console_scripts": []},
+)
